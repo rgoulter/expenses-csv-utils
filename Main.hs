5df4bda4b7ed992e443a5bcb531e714cc481e561
Richard Goulter <richard.goulter@gmail.com>
2016-05-31 21:56:53 +0800

    Tidy up main

--- Main.hs
+++ Main.hs
@@ -17,7 +17,7 @@ import ParseExpenseDirective hiding (sc)
 
 
 
-data LineDirective = DateCmd DateDirective | ExpCmd Expense deriving (Show)
+data LineDirective = DateCmd DateDirective | ExpCmd Expense deriving (Show, Eq)
 
 
 
@@ -50,14 +50,38 @@ rowFromExp :: (Int, Int, Int) -> Expense -> [String]
 rowFromExp (y,m,d) exp =
   [date, price, cur, remark, "Uncategorised"]
   where
-      date   = printf "%4d-%02d-%02d" y m d
-      amount = expenseAmount exp
-      mult = case (expenseDirection exp) of
+    date   = printf "%4d-%02d-%02d" y m d
+    amount = expenseAmount exp
+    mult   = case expenseDirection exp of
                Spent -> (1 *)
                Received -> ((-1) *)
-      price  = printf "%d.%d" (mult $ moneyDollar amount) (moneyCents amount)
-      cur    = fromMaybe "SGD" (moneyCurrency amount)
-      remark = expenseRemark exp
+    price  = printf "%d.%d" (mult $ moneyDollar amount) (moneyCents amount)
+    cur    = fromMaybe "SGD" (moneyCurrency amount)
+    remark = expenseRemark exp
+
+
+
+rowsFromDirectives :: [LineDirective] -> [[String]]
+rowsFromDirectives directives =
+  let init = ((-1, -1, -1), Mon, [])
+
+      -- Fold over a (Date, Day, GatheredRows)
+      (_, _, rows) =
+        foldl (\(date, day, rows) lineD ->
+                 case lineD of
+                   -- For ExpenseDirectives, simply add to list of 'rows'.
+                   ExpCmd exp ->
+                     (date, day, rowFromExp date exp : rows)
+
+                   -- For DateDirectives, increment/set the date/day.
+                   DateCmd dateDir ->
+                     let (date', day') = nextDate (date, day) dateDir
+                     in  (date', day', rows))
+              init
+              directives
+      rows' = reverse rows
+  in  rows'
+
 
 
 
@@ -69,16 +93,9 @@ process inputF outputF = do
   case result of
     Left err ->
       putStrLn $ parseErrorPretty err
-    Right xs -> do
-      let (_, _, rows) = foldl (\(date, day, rows) lineD -> case lineD of
-                                  ExpCmd exp -> (date, day, rowFromExp date exp : rows)
-                                  DateCmd dateDir ->
-                                    let (date', day') = nextDate (date, day) dateDir
-                                    in (date', day', rows))
-                               ((-1, -1, -1), Mon, [])
-                               xs
-          rows' = reverse rows
-          outp = printCSV rows'
-      writeFile outputF outp
+    Right directives ->
+      let rows = rowsFromDirectives directives
+          outp = printCSV rows
+      in  writeFile outputF outp
 
 
