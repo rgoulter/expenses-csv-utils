1b72844189aed5a28a2e93f4397935cc94b82a7d
Richard Goulter <richard.goulter@gmail.com>
2016-05-31 17:53:00 +0800

    Add labels to parseExpensesFile parser, improves error messages

--- Main.hs
+++ Main.hs
@@ -30,8 +30,8 @@ sc = hidden . skipMany $ choice [void spaceChar,
 
 parseExpensesFile :: Parser [LineDirective]
 parseExpensesFile =
-  some $ (sc *> (DateCmd <$> try dateDirective <* sc) <|>
-                (ExpCmd  <$> try expense <* sc))
+  some $ (sc *> (DateCmd <$> try dateDirective <* sc <?> "Date directive") <|>
+                (ExpCmd  <$> try expense <* sc <?> "Expense directive"))
 
 
 
