778c496e9fe6d3969a97b17cd844b89b6f1b7da9
Richard Goulter <richard.goulter@MX15-pro>
2018-11-12 17:37:57 +0700

    add more simple test cases

--- test/TestExpense.hs
+++ test/TestExpense.hs
@@ -10,3 +10,10 @@ expenseSpec =
     describe "numDaysAfter" $ do
       it "should compute Tue is 1 day after Mon" $ do
         (E.numDaysAfter E.Mon E.Tue) `shouldBe` (1 :: Int)
+      it "should compute Mon is 6 days after Tue" $ do
+        (E.numDaysAfter E.Tue E.Mon) `shouldBe` (6 :: Int)
+    describe "addDays" $ do
+      it "should compute 2000-01-02 is 1 day after 2000-01-01" $ do
+        (E.addDays (2000, 01, 01) 1) `shouldBe` ((2000, 01, 02) :: (Int, Int, Int))
+      it "should compute 2001-01-01 is 31 days after 2000-12-01" $ do
+        (E.addDays (2000, 12, 01) 31) `shouldBe` ((2001, 01, 01) :: (Int, Int, Int))
