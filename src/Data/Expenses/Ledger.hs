79155157c5e312b3119dedb88452813fd57d9288
Richard Goulter <richard.goulter@gmail.com>
2019-04-09 22:21:27 +0700

    output ledger file with entry's comment

--- src/Data/Expenses/Ledger.hs
+++ src/Data/Expenses/Ledger.hs
@@ -2,13 +2,15 @@
 
 module Data.Expenses.Ledger where
 
+import Data.Maybe (fromMaybe)
+
 import Data.String.Interpolate (i)
 import Data.String.Interpolate.Util (unindent)
 
 import Text.Printf (printf)
 
 import Data.Expenses.Parse.Megaparsec.Entry
-  (Entry, entryDate, entryPrice, entryRemark)
+  (Entry, entryComment, entryDate, entryPrice, entryRemark)
 import Data.Expenses.Parse.Megaparsec.ExpensesDoc (entriesFromDirectives)
 import Data.Expenses.Parse.Megaparsec.Types (LineDirective)
 
@@ -19,13 +21,14 @@ showLedgerTransactionFromEntry entry =
   unindent [i|
   #{date} #{remark}
     Undescribed  #{price} #{cur}
-    Assets:Cash:#{cur}|]
+    Assets:Cash:#{cur}|] ++ cmt
   where
     (y, m, d) = entryDate entry
     date   = printf "%4d-%02d-%02d" y m d :: String
     (dollars, cents, cur) = entryPrice entry
     price  = printf "%d.%d" dollars cents :: String
     remark = entryRemark entry
+    cmt = maybe "" ((:) '\n') $ entryComment entry
 
 
 
--- test/TestLedger.hs
+++ test/TestLedger.hs
@@ -13,9 +13,13 @@ import qualified Data.Expenses.Expense as E
 import qualified Data.Expenses.Ledger as L
 import Data.Expenses.Types (Entry(..))
 
+
+
 sampleEntry :: Entry
 sampleEntry =
   Entry (2018, 01, 01) (5, 0, "SGD") "on McDonalds" Nothing
+
+
 
 sampleTransaction :: String
 sampleTransaction =
@@ -26,9 +30,55 @@ sampleTransaction =
 
 
 
+entryWithComment :: Entry
+entryWithComment =
+  Entry (2018, 01, 01) (5, 0, "SGD") "on McDonalds" (Just "# comment")
+
+
+
+transactionWithComment :: String
+transactionWithComment =
+  unindent [i|
+  2018-01-01 on McDonalds
+    Undescribed  5.0 SGD
+    Assets:Cash:SGD
+  # comment|]
+
+
+
+entryWithMultilineComment :: Entry
+entryWithMultilineComment =
+  Entry (2018, 01, 01)
+        (5, 0, "SGD")
+        "on McDonalds"
+        (Just "# comment1\n# comment2")
+
+
+
+transactionWithMultilineComment :: String
+transactionWithMultilineComment =
+  unindent [i|
+  2018-01-01 on McDonalds
+    Undescribed  5.0 SGD
+    Assets:Cash:SGD
+  # comment1
+  # comment2|]
+
+
+
 ledgerSpec :: Spec
 ledgerSpec =
   describe "Data.Expenses.Ledger" $ do
     describe "showLedgerTransactionFromEntry" $ do
       it "should show a simple Ledger transaction for an Entry" $ do
-        L.showLedgerTransactionFromEntry sampleEntry `shouldBe` sampleTransaction
+        L.showLedgerTransactionFromEntry sampleEntry
+          `shouldBe`
+            sampleTransaction
+      it "should show a simple Ledger transaction for an Entry with a comment" $ do
+        L.showLedgerTransactionFromEntry entryWithComment
+          `shouldBe`
+            transactionWithComment
+      it "should show a simple Ledger transaction for an Entry with a multiline comment" $ do
+        L.showLedgerTransactionFromEntry entryWithMultilineComment
+          `shouldBe`
+            transactionWithMultilineComment
